c2a16b3fabae953883b5d0fde7d77c4a36f38183
AReddy <155758262+K0A1R@users.noreply.github.com>
2024-04-14 15:50:31 -0600

    Update search_books function Update library_app
    Update search_books function and added into library_app

--- search_books.py
+++ search_books.py
@@ -1,8 +1,11 @@
 def search_books(book_list,search_string):
     search_result = []
-    for book in book_list:
-        if (search_string in book):
-            search_result.append(book)
+    for b in book_list:
+        for book in b:
+            if (search_string in book.get_isbn() or search_string.lower() in book.get_title().lower() or 
+                search_string.lower() in book.get_author().lower() or search_string.lower() in book.get_genre_name().lower()):
+                print(book)
+                search_result.append(book)
     if (search_result == []):
         print('No matching books found.')
     return search_result

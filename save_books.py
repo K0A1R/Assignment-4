2339435ffd05abc561d7fb5f3fe1df6c9a45479e
AReddy <155758262+K0A1R@users.noreply.github.com>
2024-04-22 14:11:24 -0600

    Delete save_books.py

--- save_books.py
+++
@@ -1,8 +0,0 @@
-
-def save_books(book_list, path_name):
-    with open(path_name, 'w') as file:
-        for book in book_list:
-            book_info = f'{book.__str__()}\n' #currently will always save with genre name and not availability number
-            file.writelines(book_info)
-        num_of_books = len(book_list)
-        print(f'{num_of_books} books saved to the file')
